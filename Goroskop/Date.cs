3f17016881eedaf5dcd330ce9e5ec4cacd4e3eb7
Nat Sl <slupitska@gmail.com>
2023-05-19 17:39:12 +0300

    Add project files.

---
+++ Goroskop/Date.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Globalization;
+
+
+
+namespace Goroskop
+{
+    public class Date : ContentPage
+    {
+
+        public class Goroskop
+        {
+            private DateTime _dateOfBirth;
+
+            public Goroskop(DateTime dateOfBirth)
+            {
+                _dateOfBirth = dateOfBirth;
+            }
+
+            public int Age
+            {
+                get
+                {
+                    DateTime today = DateTime.Today;
+                    int age = today.Year - _dateOfBirth.Year;
+                    if (today.Month < _dateOfBirth.Month || (today.Month == _dateOfBirth.Month && today.Day < _dateOfBirth.Day))
+                    {
+                        age--;
+                    }
+                    return age;
+                }
+            }
+
+            public string ChineseZodiacSign
+            {
+                get
+                {
+                    string[] zodiacSigns = { "Обезьяна", "Петух", "Собака", "Свинья", "Крыса", "Бык", "Тигр", "Кролик", "Дракон", "Змея", "Лошадь", "Коза" };
+                    return zodiacSigns[_dateOfBirth.Year % 12];
+                }
+            }
+
+            public string WesternZodiacSign
+            {
+                get
+                {
+                    int month = _dateOfBirth.Month;
+                    int day = _dateOfBirth.Day;
+                    switch (month)
+                    {
+                        case 1:
+                            return day < 20 ? "Козерог" : "Водолей";
+                        case 2:
+                            return day < 19 ? "Водолей" : "Рыбы";
+                        case 3:
+                            return day < 21 ? "Рыбы" : "Овен";
+                        case 4:
+                            return day < 20 ? "Овен" : "Телец";
+                        case 5:
+                            return day < 21 ? "Телец" : "Близнецы";
+                        case 6:
+                            return day < 21 ? "Близнецы" : "Рак";
+                        case 7:
+                            return day < 23 ? "Рак" : "Лев";
+                        case 8:
+                            return day < 23 ? "Лев" : "Дева";
+                        case 9:
+                            return day < 23 ? "Дева" : "Весы";
+                        case 10:
+                            return day < 23 ? "Весы" : "Скорпион";
+                        case 11:
+                            return day < 22 ? "Скорпион" : "Стрелец";
+                        default:
+                            return day < 22 ? "Стрелец" : "Козерог";
+                    }
+                }
+            }
+
+            public string DateOfBirth { get; internal set; }
+            public string Birth { get; internal set; }
+
+        }
+
+        private DatePicker datePicker;
+        private Entry manualDateEntry;
+        private Button calculateButton;
+        private Button infoButton;
+        private Label birth;
+        private Label ageLabel;
+        private Label chineseZodiacLabel;
+        private Label westernZodiacLabel;
+        private Label DateOfBirth;
+        private DateTime date;
+
+        public Date()
+        {
+            // Создание DatePicker для ввода даты рождения
+            datePicker = new DatePicker()
+            {
+                Format = "dd.MM.yyyy"
+            };
+            datePicker.FontSize = 23;
+            datePicker.TextColor = Color.HotPink;
+
+            manualDateEntry = new Entry()
+            {
+                Placeholder = "Введите дату в формате dd.MM.yyyy",
+                PlaceholderColor = Color.LightSkyBlue
+            };
+            manualDateEntry.FontSize = 23;
+            manualDateEntry.TextColor = Color.Blue;
+
+            // Создание кнопки для вычисления гороскопа
+            calculateButton = new Button()
+            {
+                Text = "Рассчитать",
+                FontAttributes = FontAttributes.Bold
+            };
+            calculateButton.FontSize = 25;
+            calculateButton.TextColor = Color.White;
+            calculateButton.BackgroundColor = Color.DodgerBlue;
+
+            calculateButton.Clicked += OnCalculateClicked;
+
+            // Создание меток для вывода результатов гороскопа
+            
+            birth = new Label();
+            ageLabel = new Label();
+            chineseZodiacLabel = new Label();
+            westernZodiacLabel = new Label();
+
+            birth.FontSize = 25;
+            birth.TextColor = Color.DarkBlue;
+
+            ageLabel.FontSize = 25;
+            ageLabel.TextColor = Color.DarkBlue;
+
+            chineseZodiacLabel.FontSize = 25;
+            chineseZodiacLabel.TextColor = Color.DarkBlue;
+
+            westernZodiacLabel.FontSize = 25;
+            westernZodiacLabel.TextColor = Color.DarkBlue;
+
+            Button infoButton = new Button
+            {
+                Text = "Далее ->",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.DarkOrange,
+                BackgroundColor = Color.SpringGreen
+            };
+
+            infoButton.Clicked += async (sender, args) =>
+            {
+                DateTime dateOfBirth;
+                Goroskop goroskop = new Goroskop(datePicker.Date);
+
+                if (DateTime.TryParse(manualDateEntry.Text, out dateOfBirth))
+                {
+                    goroskop = new Goroskop(dateOfBirth);
+                }
+                else
+                {
+                    goroskop = new Goroskop(datePicker.Date);
+                }
+
+                    await Navigation.PushAsync(new Rezult(goroskop.WesternZodiacSign, goroskop.ChineseZodiacSign, goroskop.DateOfBirth, birth.Text)); //перейти на новую страницу
+            };
+
+            var inImage = new Image
+            {
+                Source = "inyan.jpg", // путь к изображению
+                //Aspect = Aspect.AspectFit,
+                WidthRequest = 150,
+                HeightRequest = 150
+            };
+
+            // Создание StackLayout для компоновки элементов на странице
+            var stackLayout = new StackLayout()
+            {
+                //настройка размера и цвета шрифта
+                Children =
+                {
+                    new Label { Text = "Выберите дату рождения из календаря:", FontSize = 20, TextColor = Color.DarkGray },
+                    datePicker,
+                    new Label { Text = "или", FontSize = 20, TextColor = Color.DarkGray },
+                    new Label { Text = "Введите дату рождения вручную:", FontSize = 20, TextColor = Color.DarkGray },
+                    manualDateEntry,
+                    calculateButton,
+                    birth,
+                    ageLabel,
+                    westernZodiacLabel,
+                    chineseZodiacLabel,
+                    infoButton,
+                    inImage
+                }
+            };
+
+            Content = stackLayout;
+        }
+
+        private async void OnCalculateClicked(object sender, EventArgs e)
+        {
+            // Получение выбранной пользователем даты рождения
+            DateTime dateOfBirth;
+
+            
+
+            if (!string.IsNullOrEmpty(manualDateEntry.Text))
+            {
+                DateTime date;
+
+                if (!DateTime.TryParseExact(manualDateEntry.Text, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+                {
+                    await DisplayAlert("Ошибка", "Пожалуйста, введите дату рождения корректно", "OK");
+
+                    birth.Text = "Пожалуйста, введите дату рождения в формате дд.ММ.гггг";
+                    ageLabel.Text = "";
+                    westernZodiacLabel.Text = "";
+                    chineseZodiacLabel.Text = "";
+                    return;
+                }
+                dateOfBirth = date;
+            }
+            else if (datePicker.Date != null)
+            {
+                dateOfBirth = datePicker.Date;
+            }
+            else
+            {
+                birth.Text = "Пожалуйста, введите дату рождения";
+                ageLabel.Text = "";
+                westernZodiacLabel.Text = "";
+                chineseZodiacLabel.Text = "";
+                return;
+            }
+
+            // Создание экземпляра класса Goroskop для вычисления результатов гороскопа
+            var goroskop = new Goroskop(dateOfBirth);
+
+            // Отображение результатов гороскопа на метках на странице
+            birth.Text = "Дата рождения: " + dateOfBirth.ToString("dd.MM.yyyy");
+            ageLabel.Text = "Ваш возраст: " + goroskop.Age;
+            westernZodiacLabel.Text = "Ваш знак Зодиака: " + goroskop.WesternZodiacSign;
+            chineseZodiacLabel.Text = "А в Китае Вы: " + goroskop.ChineseZodiacSign;
+
+            // Очистить поле ввода даты рождения, не подходит, иначе тогда не передаются данные на вторую страницу 
+            //manualDateEntry.Text = string.Empty;
+
+        }
+
+    }
+
+}
---
+++ Goroskop/Rezult.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using static Goroskop.Date;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Goroskop
+{
+    internal class Rezult : ContentPage
+    {
+        
+        //private Goroskop goroskop;
+        private Label westernZodiacLabel;
+        private Label chineseZodiacLabel;
+        private Label infoLabel;
+        private Label dopLabel;
+        //private Label DateOfBirth;
+        //private Label birth;
+
+        //private string westernZodiacSign;
+        //private string chineseZodiacSign;
+
+        // Добавляем свойства для полей westernZodiacSign и chineseZodiacSign
+        public string WesternZodiacSign { get; }
+        public string ChineseZodiacSign { get; }
+        public string DateOfBirth { get; }
+        public string Brithdate { get; }
+        public string ManualDateEntry { get; }
+        public string Goroskop { get; }
+        public string Birthdate { get; }
+
+        //public Rezult(string westernZodiacSign, string chineseZodiacSign, string DateOfBirth)
+
+        public Rezult(string westernZodiacSign, string chineseZodiacSign, string dateOfBirth, string birthdate) //birthdate - перенесли также строку с выведенной датой рождения
+        {
+            // Сохраняем значения полей в свойствах
+            WesternZodiacSign = westernZodiacSign;
+            ChineseZodiacSign = chineseZodiacSign;
+            DateOfBirth = dateOfBirth;
+            Birthdate = birthdate;
+
+            
+            infoLabel = new Label();
+            infoLabel.Text = birthdate + " и это: ";
+            infoLabel.FontSize = 25;
+            infoLabel.TextColor = Color.DodgerBlue;
+            infoLabel.HorizontalTextAlignment = TextAlignment.Center;
+
+            westernZodiacLabel = new Label();
+            chineseZodiacLabel = new Label();
+
+            westernZodiacLabel.Text = westernZodiacSign + " и " + chineseZodiacSign;
+            //chineseZodiacLabel.Text = chineseZodiacSign;
+
+            chineseZodiacLabel.FontSize = 25;
+            chineseZodiacLabel.FontAttributes = FontAttributes.Bold;
+            chineseZodiacLabel.TextColor = Color.DodgerBlue;
+            chineseZodiacLabel.HorizontalTextAlignment = TextAlignment.Center;
+
+            westernZodiacLabel.FontSize = 25;
+            westernZodiacLabel.FontAttributes = FontAttributes.Bold;
+            westernZodiacLabel.TextColor = Color.DarkOrange;
+            westernZodiacLabel.HorizontalTextAlignment = TextAlignment.Center;
+
+            dopLabel = new Label();
+            dopLabel.Text = "Прекрасное сочетание!";
+            dopLabel.FontSize = 25;
+            dopLabel.TextColor = Color.DodgerBlue;
+            dopLabel.HorizontalTextAlignment = TextAlignment.Center;
+
+            Button westernZodiacInfoButton = new Button
+            {
+                Text = westernZodiacSign, //"Зодиак",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.DarkOrange,
+                BackgroundColor = Color.SpringGreen
+            };
+            westernZodiacInfoButton.Clicked += async (sender, args) =>
+            {
+                // Формирование URL-адреса в зависимости от знака Зодиака пользователя
+                string url = "";
+
+                switch (westernZodiacSign.ToLower())
+                {
+                    case "овен":
+                        url = "https://goroskop365.ru/zodiak/aries/";
+                        break;
+                    case "телец":
+                        url = "https://goroskop365.ru/zodiak/taurus/";
+                        break;
+                    case "близнецы":
+                        url = "https://goroskop365.ru/zodiak/gemini/";
+                        break;
+                    case "рак":
+                        url = "https://goroskop365.ru/zodiak/cancer/";
+                        break;
+                    case "лев":
+                        url = "https://goroskop365.ru/zodiak/leo/";
+                        break;
+                    case "дева":
+                        url = "https://goroskop365.ru/zodiak/virgo/";
+                        break;
+                    case "весы":
+                        url = "https://goroskop365.ru/zodiak/libra/";
+                        break;
+                    case "скорпион":
+                        url = "https://goroskop365.ru/zodiak/scorpio/";
+                        break;
+                    case "стрелец":
+                        url = "https://goroskop365.ru/zodiak/sagittarius/";
+                        break;
+                    case "козерог":
+                        url = "https://goroskop365.ru/zodiak/capricorn/";
+                        break;
+                    case "водолей":
+                        url = "https://goroskop365.ru/zodiak/aquarius/";
+                        break;
+                    case "рыбы":
+                        url = "https://goroskop365.ru/zodiak/pisces/";
+                        break;
+                    default:
+                        break;
+                }
+
+                // Переход по URL-адресу в новом окне браузера
+                await Launcher.OpenAsync(new Uri(url));
+            };
+
+            Button chineseZodiacInfoButton = new Button
+            {
+                Text = chineseZodiacSign, //"Китайский гороскоп",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.DarkOrange,
+                BackgroundColor = Color.SpringGreen
+            };
+            chineseZodiacInfoButton.Clicked += async (sender, args) =>
+            {
+                string url = "";
+                switch (chineseZodiacSign)
+                {
+                    case "Крыса":
+                        url = "https://goroskop365.ru/china/krysa/";
+                        break;
+                    case "Бык":
+                        url = "https://goroskop365.ru/china/byk/";
+                        break;
+                    case "Тигр":
+                        url = "https://goroskop365.ru/china/tigr/";
+                        break;
+                    case "Кролик":
+                        url = "https://goroskop365.ru/china/zayats/";
+                        break;
+                    case "Дракон":
+                        url = "https://goroskop365.ru/china/drakon/";
+                        break;
+                    case "Змея":
+                        url = "https://goroskop365.ru/china/zmeja/";
+                        break;
+                    case "Лошадь":
+                        url = "https://goroskop365.ru/china/loshad/";
+                        break;
+                    case "Коза":
+                        url = "https://goroskop365.ru/china/koza/";
+                        break;
+                    case "Обезьяна":
+                        url = "https://goroskop365.ru/china/obezyana/";
+                        break;
+                    case "Петух":
+                        url = "https://goroskop365.ru/china/petuh/";
+                        break;
+                    case "Собака":
+                        url = "https://goroskop365.ru/china/sobaka/";
+                        break;
+                    case "Свинья":
+                        url = "https://goroskop365.ru/china/svinja/";
+                        break;
+                    default:
+                        break;
+                }
+                if (!string.IsNullOrEmpty(url))
+                {
+                    await Launcher.OpenAsync(new Uri(url));
+                }
+            };
+
+            Button combinedZodiacInfoButton = new Button
+            {
+                Text = $"{westernZodiacSign} + {chineseZodiacSign}", //"Китай + Зодиак",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.DarkOrange,
+                BackgroundColor = Color.SpringGreen
+            };
+            combinedZodiacInfoButton.Clicked += async (sender, args) =>
+            {
+                string url = "";
+                switch ($"{westernZodiacSign} + {chineseZodiacSign}")
+                {
+                    case "Стрелец + Дракон":
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/strelec-drakon/";
+                        break;
+                    case "Водолей + Крыса":
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/vodolej-krysa/";
+                        break;
+                    case "Козерог + Тигр":
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/kozerog-tigr/";
+                        break;
+                    case "Дева + Дракон":
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/deva-drakon/";
+                        break;
+                    case "Овен + Тигр":
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/oven-tigr/";
+                        break;
+
+                        //так как не все сочетания сюда прописаны - это очень утомительно и будет очень объёмный код,
+                        //то по умолчанию будет переход на общую страницу, там есть возможность выбрать свои знаки
+                    default:
+                        url = "https://goroskop365.ru/goroskop-kitaj-zodiak/";
+                        break;
+                }
+                if (!string.IsNullOrEmpty(url))
+                {
+                    await Launcher.OpenAsync(new Uri(url));
+                }
+            };
+
+            Button goroskopMayaButton = new Button
+            {
+                Text = "Гороскоп Майя", //"Китай + Зодиак",
+                FontSize = 20,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.HotPink,
+                BackgroundColor = Color.SpringGreen
+            };
+            goroskopMayaButton.Clicked += async (sender, args) =>
+            {
+                var url = "https://goroskop365.ru/maya/";
+                await Launcher.OpenAsync(new Uri(url));
+            };
+
+            var backgroundImage = new Image
+            {
+                Source = "goros.jpg", // путь к изображению
+                //Aspect = Aspect.AspectFit,
+                WidthRequest = 250,
+                HeightRequest = 250
+                //VerticalOptions = LayoutOptions.FillAndExpand,
+                //HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            Content = new StackLayout
+            {
+                //BackgroundColor = Color.Transparent, // прозрачный фон для StackLayout
+
+                Children =
+                {
+                    backgroundImage,
+                    infoLabel,
+                    westernZodiacLabel,
+                    //chineseZodiacLabel,
+                    dopLabel,
+                    westernZodiacInfoButton,
+                    chineseZodiacInfoButton,
+                    combinedZodiacInfoButton,
+                    goroskopMayaButton
+                }
+            };
+        }
+    }
+}
